9d2b5256718653c2a9097cfe56e5f9b613c1f27b
ahmedxj <66644016+ahmedxj@users.noreply.github.com>
2021-08-05 08:01:10 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,7 +1,9 @@
 
 window.addEventListener('scroll',function(){
     var scroll = document.querySelector('.backtop');
-    scroll.classList.toggle("active" , window.scrollY>500)
+	var hed = document.querySelector('header');
+    scroll.classList.toggle("active" , window.scrollY>500),
+	hed.classList.toggle("header1" , window.scrollY>300)
 })	
 
 function scrolltotp(){
@@ -11,3 +13,27 @@ function scrolltotp(){
 
 }
 
+// var bg = document.querySelector('hero');  
+var bg = document.getElementById('hero');
+var nxbtn = document.querySelector('.fa-angle-left');
+var brbtn = document.querySelector('.fa-angle-right');
+var togg = document.querySelector('.toggle-minu');
+var ulss = document.querySelector('.toggjg');
+
+nxbtn.onclick = function(){
+	bg.style.backgroundImage = 'url("img/hero/001.jpg")' ;
+};
+nxbtn.ondblclick = function(){
+	bg.style.backgroundImage = 'url("img/hero/002.jpg")' ;
+};
+
+brbtn.onclick = function(){
+	bg.style.backgroundImage = 'url("img/hero/002.jpg")' ;
+};
+brbtn.ondblclick = function(){
+	bg.style.backgroundImage = 'url("img/hero/003.jpg")' ;
+};
+
+// togg.onclick = function(){
+// 	ulss.classList.toggle("toggle1");
+// }
